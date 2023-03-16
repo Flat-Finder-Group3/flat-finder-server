73edae60576abd4565cfb38301d0bdc68d3a07d8
Mumin Ali <muminali13@hotmail.co.uk>
2023-03-16 11:03:53 +0000

    endpoint register created

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = require('./router.js')
+
+const app = express()
+
+app.use(router)
+
+app.listen(3000, () => {
+  console.log(`Listening on http://127.0.0.1:3000`)
+})
