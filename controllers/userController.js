2324d90c90f21e7ceb7634c1e80bde7672c6b773
giabertu <giabertu99@gmail.com>
2023-03-16 13:36:57 +0000

    fix: corrected extraction of request properties

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,15 @@ const supabase = require('../supabaseClient.js')
 
 async function register(req, res) {
     
-    const { name, email, password } = req
+    
+    const { name, email, password } = req.body //we had = req earlier...
 
     const { data, error } = await supabase.auth.signUp({
         email: email,
         password: password,
     })
 
-    console.log(data)
+    console.log(data, error)
 
     const user = {
         user_id: data.user.id,
