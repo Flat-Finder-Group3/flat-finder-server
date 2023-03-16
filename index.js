0a10b1df9666c8168241047acd48e22e16fd314a
giabertu <giabertu99@gmail.com>
2023-03-16 12:54:41 +0000

    fix: added cors and json parser

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
 const express = require('express')
-
 const router = require('./router.js')
-
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
 app.use(router)
 
 app.listen(3000, () => {
