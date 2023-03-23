6ab95a5cc7dd0de55c8653aafe4447174f7236cb
giabertu <giabertu99@gmail.com>
2023-03-23 16:57:39 +0000

    fix: added return of ticket when adding it

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -2,11 +2,7 @@ const supabase = require('../supabaseClient.js')
 
 async function addTicket(req, res) {
     const ticket = req.body
-
-    // console.log(ticket)
-
-    const response = await supabase.from('ticket').insert(ticket)
-
+    const response = await supabase.from('ticket').insert(ticket).select()
     res.status(200).json(response)
 }
 
