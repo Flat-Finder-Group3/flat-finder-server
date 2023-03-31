79b574e483fe96377b0dc13648ffc6174a22f2a4
giabertu <giabertu99@gmail.com>
2023-03-31 18:03:57 +0100

    fix: admin_comment was not updated

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -39,11 +39,12 @@ async function getTickets(req, res) {
 async function changeStatus(req, res) {
     const ticketID = req.body.ticketID
     const newStatus = req.body.newStatus
-    const comment = req.body.comment
+    const admin_comment = req.body.admin_comment
 
-    const response = await supabase.from('ticket').update({ status: newStatus, comment }).eq('id', ticketID)
+    const {data, error} = await supabase.from('ticket').update({ status: newStatus, admin_comment }).eq('id', ticketID).select();
 
-    res.status(200).json(response)
+    if (error) res.json(error)
+    else res.json(data)
 }
 
 module.exports = {
